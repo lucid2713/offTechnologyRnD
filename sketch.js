c03353c4c69e65d3b53d115cab43c26ec73b17d2
Mina Kim <lucid2713@gmail.com>
2019-05-10 01:03:55 +0200

    updated

--- sketch.js
+++ sketch.js
@@ -1,30 +1,16 @@
 function setup(){
   createCanvas(screen.width, screen.height);
   stroke('#00FF7F');
-  // fill(0);
   noFill();
   frameRate(20);
   rectMode(CENTER);
 }
 
 function draw(){
-  if (mouseIsPressed) {
-      // fill('#00FF7F');
-      stroke('#FF143C');
-    } else {
-      stroke('#00FF7F');
-    }
-    // ellipse(mouseX, mouseY, 80, 80);
+  // if (mouseIsPressed) {
+  //     stroke('#FF143C');
+  //   } else {
+  //     stroke('#00FF7F');
+  //   }
     rect(mouseX, mouseY, 80, 80);
-    // rect(50, 100, 200, 150);
-    // rect(300, 150, 200, 150);
-    // rect(550, 130, 200, 150);
-  // background(204);
-  // if (mouseX < 50) {
-  //   rect(0, 0, 50, 100);  // Left
-  // }
-  // else {
-  //   rect(50, 0, 50, 100); // Right
-  // }
-
 }
